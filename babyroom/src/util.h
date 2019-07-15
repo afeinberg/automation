739cf6e503629fb2aa5d933dca3ebcf0a86e1b99
Alex Feinberg <alex@strlen.net>
2019-07-15 06:25:13 -0700

    Cleanup util.h

--- babyroom/src/util.h
+++ babyroom/src/util.h
@@ -6,30 +6,34 @@
 #include "Arduino.h"
 
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&) = delete;      \
-  void operator=(const TypeName&) = delete
+  TypeName(const TypeName &) = delete;     \
+  void operator=(const TypeName &) = delete
 
-enum LogLevel {
+enum LogLevel
+{
   INFO = 0,
   DEBUG = 1
 };
 
-template<typename ... Types>
-void SerialLog(LogLevel level, Types ... rest);
+template <typename... Types>
+void SerialLog(LogLevel level, Types... rest);
 
-template<typename T>
-void SeriaLog(LogLevel level, T t) {
+template <typename T>
+void SeriaLog(LogLevel level, T t)
+{
   Serial.print(t);
 }
 
-template<>
-void SerialLog(LogLevel level) {
+template <>
+void SerialLog(LogLevel level)
+{
   Serial.print(level == INFO ? " INFO" : " DEBUG");
   Serial.println();
 }
 
-template<typename T1, typename ... Types>
-void SerialLog(LogLevel level, T1 t, Types ... rest) {
+template <typename T1, typename... Types>
+void SerialLog(LogLevel level, T1 t, Types... rest)
+{
   Serial.print(t);
   Serial.print(" ");
   SerialLog(level, rest...);
